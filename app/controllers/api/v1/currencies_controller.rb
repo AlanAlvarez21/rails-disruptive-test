329f433fed3db976045c569eec0c99ed7a99c1cd
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-01-30 01:18:25 -0600

    feat: apply linter to all code

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,52 +1,59 @@
-# Currencies Controller 
-class Api::V1::CurrenciesController < ApplicationController
-  before_action :set_currency, only: %i[ show update destroy ]
+# frozen_string_literal: true
 
-  # GET /currencies
-  def index
-    @currencies = Currency.all
+# Currencies Controller
+module Api
+  module V1
+    class CurrenciesController < ApplicationController
+      before_action :set_currency, only: %i[show update destroy]
 
-    render json: @currencies
-  end
+      # GET /currencies
+      def index
+        @currencies = Currency.all
 
-  # GET /currencies/1
-  def show
-    render json: @currency
-  end
+        render json: @currencies
+      end
 
-  # POST /currencies
-  def create
-    @currency = Currency.new(currency_params)
+      # GET /currencies/1
+      def show
+        render json: @currency
+      end
 
-    if @currency.save
-      render json: @currency, status: :created, location: @currency
-    else
-      render json: @currency.errors, status: :unprocessable_entity
+      # POST /currencies
+      def create
+        @currency = Currency.new(currency_params)
+
+        if @currency.save
+          render json: @currency, status: :created, location: @currency
+        else
+          render json: @currency.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /currencies/1
+      def update
+        if @currency.update(currency_params)
+          render json: @currency
+        else
+          render json: @currency.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /currencies/1
+      def destroy
+        @currency.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_currency
+        @currency = Currency.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def currency_params
+        params.require(:currency).permit(:title, :body)
+      end
     end
   end
-
-  # PATCH/PUT /currencies/1
-  def update
-    if @currency.update(currency_params)
-      render json: @currency
-    else
-      render json: @currency.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /currencies/1
-  def destroy
-    @currency.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_currency
-      @currency = Currency.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def currency_params
-      params.require(:currency).permit(:title, :body)
-    end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://127.0.0.1:5173'
-      resource '*',
-               headers: :any,
-               methods: %i[get post put patch delete options head]
-    end
+  allow do
+    origins 'http://127.0.0.1:5173'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end
-  
