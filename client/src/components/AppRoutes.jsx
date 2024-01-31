31b816dbfeace8241c6f5a9c4d8afc5ce01140e9
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-01-30 21:08:28 -0600

    feat: add router and AppRoutes component

---
+++ client/src/components/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import {CurrencyList, CurrencyDetails} from './';
+
+function AppRoutes() {
+  return(
+    <Routes>
+        <Route path="/" element={<CurrencyList/>}></Route>
+        <Route path="/new" element={<h1>Nueva moneda!</h1>}></Route>
+        <Route path="currency/:id" element={<CurrencyDetails />}></Route>
+    </Routes>
+  );
+}
+
+
+export default AppRoutes
